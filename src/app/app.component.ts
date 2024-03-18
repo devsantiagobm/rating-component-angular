106c8ca6a05bc4ba71e0f0ce71c91476faf6a35c
User <sanseb290514@gmail.com>
2024-03-17 19:36:04 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
 
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'bases';
+  ratings = [1, 2, 3, 4, 5];
+  selected: number | null = null;
+  canShowResult = false;
+
+  setSelected(rating: number) {
+    this.selected = rating;
+  }
+
+  showResult() {
+    if (this.selected) this.canShowResult = true;
+  }
+  
 }
